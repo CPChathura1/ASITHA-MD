5db455317fa349f8433104daae9a140ca2b49aec
CPChathura1 <githubnewcp@gmail.com>
2024-10-05 21:42:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "BQ50XAAR#flx6ZqtRPLgX48uZLLfZImTL_1DuBugggJYmXlheB2E",
+SESSION_ID: process.env.SESSION_ID || "Se4SQRIJ#OHi-5Ivs0jZ8JD-7KYW9bzAgKRXOuQ4J9JNfbY1e8l4",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
